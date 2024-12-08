da8e91b06afc65e53f79ddc3e4ba4b7f6b8e1d29
Jorge Raphael <44186391+JorgeRaphael@users.noreply.github.com>
2024-12-08 20:45:44 -0300

    Update getDiceToPush.tsx

--- src/helpers/getDiceToPush.tsx
+++ src/helpers/getDiceToPush.tsx
@@ -19,7 +19,7 @@ export function getDiceToPush(
         const value = values[dieOrDice.id];
         console.log (dieOrDice.style + " " + dieOrDice.type + ": " + value);
         if (value !== undefined) {
-            if (dieOrDice.style === "MYZGEAR" && value > 1 && value < 6 || "MYZBASE" && value > 1 && value < 6) {
+            /*if (dieOrDice.style === "MYZGEAR" && value > 1 && value < 6 || "MYZBASE" && value > 1 && value < 6) {
                 if (dieOrDice.type === "D6" ||
                     dieOrDice.type === "D8" ||
                     dieOrDice.type === "D10" ||
@@ -28,7 +28,7 @@ export function getDiceToPush(
                     diceToPush.push(dieOrDice.id);
                 }
             }
-            else if (dieOrDice.style === "MYZSKILL" && dieOrDice.type === "D6" && value < 6) {
+            else */ if (dieOrDice.style === "MYZSKILL" && dieOrDice.type === "D6" && value < 6) {
                 diceToPush.push(dieOrDice.id);
             }
         }
