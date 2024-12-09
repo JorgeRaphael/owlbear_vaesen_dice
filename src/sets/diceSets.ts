8de41d6ee8acc3db0e235726d37e93ff7728272e
Jorge Raphael <44186391+JorgeRaphael@users.noreply.github.com>
2024-12-08 22:19:06 -0300

    Update diceSets.ts

--- src/sets/diceSets.ts
+++ src/sets/diceSets.ts
@@ -9,9 +9,9 @@ import * as myzgearPreviews from "../previews/myzgear";
 import allPreview from "../previews/all.png";
 
 const standardPreviews: Record<DiceStyle, string> = {
-  //MYZBASE: myzbasePreviews.D6,
+  MYZBASE: myzbasePreviews.D6,
   MYZSKILL: myzskillPreviews.D6,
-  //MYZGEAR: myzgearPreviews.D6,
+  MYZGEAR: myzgearPreviews.D6,
 };
 
 function createStandardSet(style: DiceStyle): DiceSet {
